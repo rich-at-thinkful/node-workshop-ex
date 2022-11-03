15f8aa06b92eda391f044da52534b7e3706b8370
Rich Greenhill <rgreenhill@thinkful.com>
2022-11-03 13:51:29 -0700

    update knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,9 @@
 const path = require("path");
 require("dotenv").config();
-const { DATABASE_URL } = process.env;
+const DATABASE_URL =
+  process.env.NODE_ENV === "development"
+    ? process.env.DATABASE_URL_DEV
+    : process.env.DATABASE_URL_PROD;
 
 module.exports = {
   development: {
