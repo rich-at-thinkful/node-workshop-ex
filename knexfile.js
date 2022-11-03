83d267bbd9c125f9aa763b299d1089d9a625eeaf
Rich Greenhill <rgreenhill@thinkful.com>
2022-11-03 13:21:10 -0700

    add production env to knexfile

--- knexfile.js
+++ knexfile.js
@@ -13,4 +13,15 @@ module.exports = {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
   },
+
+  production: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  }
 };
